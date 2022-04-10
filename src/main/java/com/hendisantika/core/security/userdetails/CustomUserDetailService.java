a9ca7b3743673d1e8f8dfae48b16f51eaa8b1524
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:21:54 +0700

    #61 - Add loadUserByUsername in CustomUserDetailService

--- src/main/java/com/hendisantika/core/security/userdetails/CustomUserDetailService.java
+++ src/main/java/com/hendisantika/core/security/userdetails/CustomUserDetailService.java
@@ -1,7 +1,9 @@
 package com.hendisantika.core.security.userdetails;
 
+import com.hendisantika.core.user.entity.UserEntity;
 import com.hendisantika.core.user.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,4 +24,25 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Resource
     UserRepository userRepository;
+
+    @Override
+    public CustomUser loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        final UserEntity customer = userRepository.findByEmail(email);
+        if (customer == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        boolean enabled = !customer.isAccountVerified(); // we can use this in case we want to activate account after
+        // customer verified the account
+        CustomUser user = CustomUser.CustomUserBuilder.aCustomUser().
+                withUsername(customer.getEmail())
+                .withPassword(customer.getPassword())
+                .withEnabled(customer.isLoginDisabled())
+                .withAuthorities(getAuthorities(customer))
+                .withSecret(customer.getSecret())
+                .withAccountNonLocked(false)
+                .build();
+
+        return user;
+    }
 }
