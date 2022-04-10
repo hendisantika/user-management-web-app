926160908da32460d002db1ff6d41483de2795f2
Hendi Santika <hendisantika@gmail.com>
2022-04-10 13:49:23 +0700

    #47 - Add BruteForceProtectionService

---
+++ src/main/java/com/hendisantika/core/security/bruteforce/BruteForceProtectionService.java
@@ -0,0 +1,38 @@
+package com.hendisantika.core.security.bruteforce;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/22
+ * Time: 13.48
+ */
+public interface BruteForceProtectionService {
+    /**
+     * Method to register every login failure attempt for a given username.
+     * We will be using a simple counter to keep track of the failed attempts.
+     *
+     * @param username
+     */
+    void registerLoginFailure(final String username);
+
+    /**
+     * Method to reset the counter for successful login.
+     * We want to make sure that we are setting the counter as 0
+     * on successful login to avoid customer lockout.
+     *
+     * @param username
+     */
+    void resetBruteForceCounter(final String username);
+
+    /**
+     * check if the user account is under brute force attack, this will check the failed count with
+     * threshold value and will return true if the failed count exceeds the threshold value.
+     *
+     * @param username
+     * @return
+     */
+    boolean isBruteForceAttack(final String username);
+}
