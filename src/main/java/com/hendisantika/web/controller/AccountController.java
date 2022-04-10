8bbc5b8ed803cfc92034d53dc9002b42e08fa701
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:45:56 +0700

    #105 - Add home & dummyCookie in AccountController

--- src/main/java/com/hendisantika/web/controller/AccountController.java
+++ src/main/java/com/hendisantika/web/controller/AccountController.java
@@ -1,10 +1,17 @@
 package com.hendisantika.web.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,20 @@ public class AccountController {
 
     @Resource
     private SessionRegistry sessionRegistry;
+
+    @GetMapping("/home")
+    public String home(HttpServletResponse response, Authentication authentication, Model model) {
+        setDummyCookie(response);
+        List<SessionInformation> sessions = sessionRegistry.getAllSessions
+                (authentication.getPrincipal(), false);
+        model.addAttribute("currentSession", sessions);
+        return "index";
+    }
+
+    private void setDummyCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("dummyCookie", "dummy_cookie");
+        cookie.setMaxAge(2592000);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
 }
