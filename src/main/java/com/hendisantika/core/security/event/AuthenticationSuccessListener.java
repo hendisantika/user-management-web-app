bb37c343f47df9070d41c0a1d38cba7f534d2f9f
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:23:36 +0700

    #64 - Add AuthenticationSuccessListener

---
+++ src/main/java/com/hendisantika/core/security/event/AuthenticationSuccessListener.java
@@ -0,0 +1,35 @@
+package com.hendisantika.core.security.event;
+
+import com.hendisantika.core.security.bruteforce.BruteForceProtectionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/04/22
+ * Time: 05.23
+ */
+@Component
+public class AuthenticationSuccessListener implements ApplicationListener<AuthenticationSuccessEvent> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AuthenticationSuccessListener.class);
+
+    @Resource(name = "bruteForceProtectionService")
+    private BruteForceProtectionService bruteForceProtectionService;
+
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        String username = event.getAuthentication().getName();
+        LOG.info("********* login successful for user {} ", username);
+        bruteForceProtectionService.resetBruteForceCounter(username);
+    }
+}
