ad86b22469c5f8b767dbb2ffd5bc836643893858
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:26:20 +0700

    #69 - Add CustomAccessDeniedHandler

---
+++ src/main/java/com/hendisantika/core/security/handlers/CustomAccessDeniedHandler.java
@@ -0,0 +1,41 @@
+package com.hendisantika.core.security.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/04/22
+ * Time: 05.25
+ */
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+
+            LOG.info("User '" + authentication.getName()
+                    + "' attempted to access the URL: "
+                    + request.getRequestURI());
+        }
+        response.sendRedirect(request.getContextPath() + "/access-denied");
+    }
+}
