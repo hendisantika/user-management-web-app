7e3785a8f065367525c06ceafcc3ecb2c46d1598
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:24:39 +0700

    #66 - Add CustomAuthenticationFilter in CustomAuthenticationFilter

--- src/main/java/com/hendisantika/core/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/hendisantika/core/security/filter/CustomAuthenticationFilter.java
@@ -1,6 +1,12 @@
 package com.hendisantika.core.security.filter;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +20,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private static final String customToken = "jdjCustomToken";
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        UsernamePasswordAuthenticationToken authRequest = getAuth(request);
+
+        return super.attemptAuthentication(request, response);
+    }
 }
