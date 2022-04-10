9a28c88fe7f77c1154e5449ab14814d529a92f74
Hendi Santika <hendisantika@gmail.com>
2022-04-10 13:29:34 +0700

    #17 - Add Group Entity

---
+++ src/main/java/com/hendisantika/core/user/entity/Group.java
@@ -0,0 +1,35 @@
+package com.hendisantika.core.user.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/22
+ * Time: 13.28
+ */
+@Entity
+@Table(name = "principle_groups")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+    private String name;
+
+    @ManyToMany(mappedBy = "userGroups")
+    private Set<UserEntity> users;
+}
