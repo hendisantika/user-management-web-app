b37e023abd6c677b4feeaba6a91c2fbfd9b9efe3
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:32:48 +0700

    #82 - Add createSecureToken in DefaultSecureTokenService

--- src/main/java/com/hendisantika/core/security/token/DefaultSecureTokenService.java
+++ src/main/java/com/hendisantika/core/security/token/DefaultSecureTokenService.java
@@ -1,12 +1,16 @@
 package com.hendisantika.core.security.token;
 
+import com.hendisantika.core.security.jpa.SecureToken;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.keygen.BytesKeyGenerator;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +30,15 @@ public class DefaultSecureTokenService implements SecureTokenService {
     SecureTokenRepository secureTokenRepository;
     @Value("${jdj.secure.token.validity}")
     private int tokenValidityInSeconds;
+
+    @Override
+    public SecureToken createSecureToken() {
+        String tokenValue = new String(Base64.encodeBase64URLSafe(DEFAULT_TOKEN_GENERATOR.generateKey()), US_ASCII);
+        // this is a sample, you can adapt as per your security need
+        SecureToken secureToken = new SecureToken();
+        secureToken.setToken(tokenValue);
+        secureToken.setExpireAt(LocalDateTime.now().plusSeconds(getTokenValidityInSeconds()));
+        this.saveSecureToken(secureToken);
+        return secureToken;
+    }
 }
