54ffaca1917a96cb2ef3c33fe9ea84f3e1d5d5ec
Hendi Santika <hendisantika@gmail.com>
2022-04-10 11:05:18 +0700

    #12 - Set servletContainer in Main App

--- src/main/java/com/hendisantika/UserManagementWebAppApplication.java
+++ src/main/java/com/hendisantika/UserManagementWebAppApplication.java
@@ -1,7 +1,12 @@
 package com.hendisantika;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserManagementWebAppApplication {
@@ -10,4 +15,21 @@ public class UserManagementWebAppApplication {
         SpringApplication.run(UserManagementWebAppApplication.class, args);
     }
 
+    //HTTP port
+    @Value("${http.port}")
+    private int httpPort;
+
+    // Let's configure additional connector to enable support for both HTTP and HTTPS
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
 }
