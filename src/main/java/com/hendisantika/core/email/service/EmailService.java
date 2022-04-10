96ee32ed732fd23176815e635942da9b1893fe6c
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:40:13 +0700

    #98 - Add EmailService

---
+++ src/main/java/com/hendisantika/core/email/service/EmailService.java
@@ -0,0 +1,19 @@
+package com.hendisantika.core.email.service;
+
+import com.hendisantika.core.email.context.AbstractEmailContext;
+
+import javax.mail.MessagingException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/04/22
+ * Time: 05.39
+ */
+public interface EmailService {
+
+    void sendMail(final AbstractEmailContext email) throws MessagingException;
+}
