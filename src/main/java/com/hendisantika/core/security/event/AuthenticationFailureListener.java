bafe5d9a15b2a28ccd1075860a61b7cff039edd4
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:23:02 +0700

    #63 - Add AuthenticationFailureListener

---
+++ src/main/java/com/hendisantika/core/security/event/AuthenticationFailureListener.java
@@ -0,0 +1,34 @@
+package com.hendisantika.core.security.event;
+
+import com.hendisantika.core.security.bruteforce.BruteForceProtectionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/04/22
+ * Time: 05.22
+ */
+@Component
+public class AuthenticationFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AuthenticationFailureListener.class);
+
+    @Resource(name = "bruteForceProtectionService")
+    private BruteForceProtectionService bruteForceProtectionService;
+
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
+        String username = event.getAuthentication().getName();
+        bruteForceProtectionService.registerLoginFailure(username);
+    }
+}
