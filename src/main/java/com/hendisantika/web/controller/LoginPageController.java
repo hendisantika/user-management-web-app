0e8dadd52974a36f9b86435e607b9253c8624f27
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:47:30 +0700

    #108 - Add login in LoginPageController

--- src/main/java/com/hendisantika/web/controller/LoginPageController.java
+++ src/main/java/com/hendisantika/web/controller/LoginPageController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.web.controller;
 
 import com.hendisantika.core.service.CustomerAccountService;
+import com.hendisantika.web.data.user.ResetPasswordData;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +30,25 @@ public class LoginPageController {
     @Resource(name = "customerAccountService")
     private CustomerAccountService customerAccountService;
 
+    @GetMapping
+    public String login(@RequestParam(value = "error", defaultValue = "false") boolean loginError,
+                        @RequestParam(value = "invalid-session", defaultValue = "false") boolean invalidSession,
+                        final Model model, HttpSession session) {
+
+        String userName = getUserName(session);
+        if (loginError) {
+            if (StringUtils.isNotEmpty(userName) && customerAccountService.loginDisabled(userName)) {
+                model.addAttribute("accountLocked", Boolean.TRUE);
+                model.addAttribute("forgotPassword", new ResetPasswordData());
+                return "account/login";
+            }
+        }
+        if (invalidSession) {
+            model.addAttribute("invalidSession", "You already have an active session. We do not allow multiple active" +
+                    " sessions");
+        }
+        model.addAttribute("forgotPassword", new ResetPasswordData());
+        model.addAttribute("accountLocked", Boolean.FALSE);
+        return "account/login";
+    }
 }
