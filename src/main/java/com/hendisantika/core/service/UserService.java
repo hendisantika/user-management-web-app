af051c8026033d37a493592e6355f7000849cc61
Hendi Santika <hendisantika@gmail.com>
2022-04-10 13:39:34 +0700

    #31 - Add UserService in DefaultUserService

---
+++ src/main/java/com/hendisantika/core/service/UserService.java
@@ -0,0 +1,30 @@
+package com.hendisantika.core.service;
+
+import com.hendisantika.core.exception.InvalidTokenException;
+import com.hendisantika.core.exception.UnknownIdentifierException;
+import com.hendisantika.core.exception.UserAlreadyExistException;
+import com.hendisantika.core.user.entity.UserEntity;
+import dev.samstevens.totp.exceptions.QrGenerationException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/22
+ * Time: 13.39
+ */
+public interface UserService {
+    void register(final UserData user) throws UserAlreadyExistException;
+
+    boolean checkIfUserExist(final String email);
+
+    void sendRegistrationConfirmationEmail(final UserEntity user);
+
+    boolean verifyUser(final String token) throws InvalidTokenException;
+
+    UserEntity getUserById(final String id) throws UnknownIdentifierException;
+
+    MfaTokenData mfaSetup(final String email) throws UnknownIdentifierException, QrGenerationException;
+}
