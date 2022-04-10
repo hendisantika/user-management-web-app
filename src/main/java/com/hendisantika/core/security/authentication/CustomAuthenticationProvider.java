9f5208c429bba520e24009cc1530517b63c1623d
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:20:12 +0700

    #58 - Add additionalAuthenticationChecks in CustomAuthenticationProvider

--- src/main/java/com/hendisantika/core/security/authentication/CustomAuthenticationProvider.java
+++ src/main/java/com/hendisantika/core/security/authentication/CustomAuthenticationProvider.java
@@ -1,7 +1,11 @@
 package com.hendisantika.core.security.authentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -29,5 +33,23 @@ public class CustomAuthenticationProvider extends DaoAuthenticationProvider {
     @Autowired
     public CustomAuthenticationProvider(UserDetailsService userDetailsService) {
         super.setUserDetailsService(userDetailsService);
+    }
+
+    protected void additionalAuthenticationChecks(UserDetails userDetails,
+                                                  UsernamePasswordAuthenticationToken authentication)
+            throws AuthenticationException {
+
+        super.additionalAuthenticationChecks(userDetails, authentication);
+
+        //token check
+        CustomWebAuthenticationDetails authenticationDetails =
+                (CustomWebAuthenticationDetails) authentication.getDetails();
+        CustomUser user = (CustomUser) userDetails;
+        String mfaToken = authenticationDetails.getToken();
+        if (!mfaTokenManager.verifyTotp(mfaToken, user.getSecret())) {
+            throw new BadCredentialsException(messages.getMessage(
+                    "AbstractUserDetailsAuthenticationProvider.badCredentials",
+                    "Bad credentials"));
+        }
     }
 }
