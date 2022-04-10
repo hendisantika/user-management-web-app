7ae5e886caf498d96193e4a7b814b7b810c78232
Hendi Santika <hendisantika@gmail.com>
2022-04-10 13:27:48 +0700

    #16 - Add UserGroupType Enum

---
+++ src/main/java/com/hendisantika/core/user/UserGroupType.java
@@ -0,0 +1,16 @@
+package com.hendisantika.core.user;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/22
+ * Time: 13.27
+ */
+public enum UserGroupType {
+    ADMIN,
+    CUSTOMER,
+    EDITOR
+}
