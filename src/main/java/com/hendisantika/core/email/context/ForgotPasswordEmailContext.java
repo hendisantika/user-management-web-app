3fc653c2628ee5f6cbd6862b7a3f786f12cd2e01
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:39:02 +0700

    #95 - Add ForgotPasswordEmailContext

---
+++ src/main/java/com/hendisantika/core/email/context/ForgotPasswordEmailContext.java
@@ -0,0 +1,15 @@
+package com.hendisantika.core.email.context;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/04/22
+ * Time: 05.38
+ */
+public class ForgotPasswordEmailContext extends AbstractEmailContext {
+
+    private String token;
+}
