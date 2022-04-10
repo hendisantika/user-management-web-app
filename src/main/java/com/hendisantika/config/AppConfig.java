f15e41a59a2b16a405f1b796c4fb66de2d347b26
Hendi Santika <hendisantika@gmail.com>
2022-04-10 10:59:43 +0700

    #5 - Add passwordEncoder Bean in AppConfig

--- src/main/java/com/hendisantika/config/AppConfig.java
+++ src/main/java/com/hendisantika/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.hendisantika.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,4 +17,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // with new spring security 5
+    }
 }
