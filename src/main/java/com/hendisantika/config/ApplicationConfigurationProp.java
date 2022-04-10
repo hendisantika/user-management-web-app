2b72a695200dd5a34e9f19e89b5c7e0068597380
Hendi Santika <hendisantika@gmail.com>
2022-04-10 11:02:30 +0700

    #10 - Add ApplicationConfigurationProp

---
+++ src/main/java/com/hendisantika/config/ApplicationConfigurationProp.java
@@ -0,0 +1,17 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/22
+ * Time: 11.02
+ */
+@Configuration
+public class ApplicationConfigurationProp extends AbstractSecurityWebApplicationInitializer {
+}
