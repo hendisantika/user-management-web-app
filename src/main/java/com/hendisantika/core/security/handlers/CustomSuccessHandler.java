3c44fb8faf8889b0f303e7d38937fcbb061fe78b
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:27:14 +0700

    #71 - Add onAuthenticationSuccess in CustomSuccessHandler

--- src/main/java/com/hendisantika/core/security/handlers/CustomSuccessHandler.java
+++ src/main/java/com/hendisantika/core/security/handlers/CustomSuccessHandler.java
@@ -1,8 +1,15 @@
 package com.hendisantika.core.security.handlers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +24,25 @@ public class CustomSuccessHandler extends SavedRequestAwareAuthenticationSuccess
 
     private final GrantedAuthority adminAuthority = new SimpleGrantedAuthority(
             "ROLE_ADMIN");
+
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        if (isAdminAuthority(authentication)) {
+            String targetUrl = "home/admin";
+            clearAuthenticationAttributes(request);
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        } else {
+            String targetUrl = determineTargetUrl(request, response); // using the super class to do this work for us
+
+            // this logic is only for demo purpose, please do not use it on production application.
+            if (StringUtils.isEmpty(targetUrl) || StringUtils.equals(targetUrl, "/")) {
+                targetUrl = "/account/home";
+            }
+            clearAuthenticationAttributes(request);
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+            //You can let Spring security handle it for you.
+            // super.onAuthenticationSuccess(request, response, authentication);
+        }
+    }
 }
