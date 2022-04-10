78399430ab47d6ed53d450184e5bb5a70018b385
Hendi Santika <hendisantika@gmail.com>
2022-04-10 10:59:18 +0700

    #4 - Add AppConfig

---
+++ src/main/java/com/hendisantika/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/22
+ * Time: 10.58
+ */
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+}
