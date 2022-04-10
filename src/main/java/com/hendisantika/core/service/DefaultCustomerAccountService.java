de9ddf65df7f80f1aae6f0ae545d8c4d1e5e6cb4
Hendi Santika <hendisantika@gmail.com>
2022-04-10 13:40:42 +0700

    #32 - Add DefaultCustomerAccountService

---
+++ src/main/java/com/hendisantika/core/service/DefaultCustomerAccountService.java
@@ -0,0 +1,39 @@
+package com.hendisantika.core.service;
+
+import com.hendisantika.core.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/04/22
+ * Time: 13.40
+ */
+@Service("customerAccountService")
+public class DefaultCustomerAccountService implements CustomerAccountService {
+
+    @Resource
+    UserService userService;
+    @Resource
+    SecureTokenRepository secureTokenRepository;
+    @Resource
+    private SecureTokenService secureTokenService;
+    @Value("${site.base.url.https}")
+    private String baseURL;
+
+    @Resource
+    private EmailService emailService;
+
+    @Resource
+    private UserRepository userRepository;
+
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
+}
