b9ed796c25e260e0a64a7cfc3a3c9f88f847b550
Hendi Santika <hendisantika@gmail.com>
2022-04-10 11:01:55 +0700

    #9 - Add secretGenerator, qrGenerator & codeVerifier Bean in AppConfig

--- src/main/java/com/hendisantika/config/AppConfig.java
+++ src/main/java/com/hendisantika/config/AppConfig.java
@@ -1,5 +1,13 @@
 package com.hendisantika.config;
 
+import dev.samstevens.totp.code.CodeVerifier;
+import dev.samstevens.totp.code.DefaultCodeGenerator;
+import dev.samstevens.totp.code.DefaultCodeVerifier;
+import dev.samstevens.totp.qr.QrGenerator;
+import dev.samstevens.totp.qr.ZxingPngQrGenerator;
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
+import dev.samstevens.totp.time.SystemTimeProvider;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -66,5 +74,22 @@ public class AppConfig implements WebMvcConfigurer {
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource);
         return bean;
+    }
+
+    // setting up thymeleaf for Spring Email TemplateEngine
+
+    @Bean
+    public SecretGenerator secretGenerator() {
+        return new DefaultSecretGenerator(64);
+    }
+
+    @Bean
+    public QrGenerator qrGenerator() {
+        return new ZxingPngQrGenerator();
+    }
+
+    @Bean
+    public CodeVerifier codeVerifier() {
+        return new DefaultCodeVerifier(new DefaultCodeGenerator(), new SystemTimeProvider());
     }
 }
