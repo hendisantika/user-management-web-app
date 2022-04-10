1fe27689305abbd88716c28528b840363ff6a0b5
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:38:15 +0700

    #93 - Add AbstractEmailContext

---
+++ src/main/java/com/hendisantika/core/email/context/AbstractEmailContext.java
@@ -0,0 +1,121 @@
+package com.hendisantika.core.email.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/04/22
+ * Time: 05.37
+ */
+public abstract class AbstractEmailContext {
+
+    private String from;
+    private String to;
+    private String subject;
+    private String email;
+    private String attachment;
+    private String fromDisplayName;
+    private String emailLanguage;
+    private String displayName;
+    private String templateLocation;
+    private Map<String, Object> context;
+
+
+    public AbstractEmailContext() {
+        this.context = new HashMap<>();
+    }
+
+    public <T> void init(T context) {
+        //we can do any common configuration setup here
+        // like setting up some base URL and context
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public String getFromDisplayName() {
+        return fromDisplayName;
+    }
+
+    public void setFromDisplayName(String fromDisplayName) {
+        this.fromDisplayName = fromDisplayName;
+    }
+
+    public String getEmailLanguage() {
+        return emailLanguage;
+    }
+
+    public void setEmailLanguage(String emailLanguage) {
+        this.emailLanguage = emailLanguage;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Map<String, Object> getContext() {
+        return context;
+    }
+
+    public void setContext(Map<String, Object> context) {
+        this.context = context;
+    }
+
+    public Object put(String key, Object value) {
+        return key == null ? null : this.context.put(key.intern(), value);
+    }
+
+    public String getTemplateLocation() {
+        return templateLocation;
+    }
+
+    public void setTemplateLocation(String templateLocation) {
+        this.templateLocation = templateLocation;
+    }
+}
