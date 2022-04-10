1a4caa1c183b41976cc7a93517714753c16841c0
Hendi Santika <hendisantika@gmail.com>
2022-04-11 05:37:20 +0700

    #92 - Add CustomWebAuthenticationDetailsSource

---
+++ src/main/java/com/hendisantika/core/security/web/authentication/CustomWebAuthenticationDetailsSource.java
@@ -0,0 +1,26 @@
+package com.hendisantika.core.security.web.authentication;
+
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : user-management-web-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/04/22
+ * Time: 05.37
+ */
+@Component
+public class CustomWebAuthenticationDetailsSource implements AuthenticationDetailsSource<HttpServletRequest,
+        WebAuthenticationDetails> {
+
+    @Override
+    public CustomWebAuthenticationDetails buildDetails(HttpServletRequest context) {
+        return new CustomWebAuthenticationDetails(context);
+    }
+}
